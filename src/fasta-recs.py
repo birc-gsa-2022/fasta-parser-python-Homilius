dbe997e6d45898fb7b7aa56fbbbe5fcbf311bcd4
Anton Homilius <homilius@antons-air-2.eduroam.net.au.dk>
2022-08-31 14:49:24 +0200

    going old school

--- src/fasta-recs.py
+++ src/fasta-recs.py
@@ -1,16 +1,32 @@
-import argparse
+#!/usr/bin/python3
 
+import sys
 
-def main():
-    argparser = argparse.ArgumentParser(
-        description="Extract Simple-FASTA records"
-    )
-    argparser.add_argument(
-        "fasta",
-        type=argparse.FileType('r')
-    )
-    args = argparser.parse_args() 
+def read_fasta():
+    # load input:
+    inFile = sys.argv[1]  # The list of command line arguments. e.g sys.argv[0] is the script name itself.
+    with open(inFile,'r') as j:
+        lines = j.readlines()
     
+    # make fasta parser:
+    record_list = []
+    header = ''
+    sequence = ''
+    for line in lines:
+        line = line.strip()
+        if line.startswith('>'):
+            if header != "":
+                record_list.append([header, sequence])
+                sequence = ""
+            header = line[1:]
+        else:
+            sequence = sequence + line
+    record_list.append([header, sequence])
 
-if __name__ == '__main__':
-    main()
+    # clean output:
+    for i in record_list:
+        #print('> ' + i[0].strip() + '\t' + i[1]) 
+        print(i[0].strip() + '\t' + i[1]) 
+    
+    return ''
+read_fasta()
