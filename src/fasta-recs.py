8ec4eab2d6a71f20d33b5304dc069053266d0c85
Anton Homilius <antonhomilius@hotmail.com>
2022-09-03 21:08:24 +0200

    some_correction

--- src/fasta-recs.py
+++ src/fasta-recs.py
@@ -3,23 +3,23 @@ import sys
 def read_fasta():
     # load input:
     inFile = sys.argv[1]
-    with open(inFile,'r') as j:
-        lines = j.readlines()
+    with open(inFile,'r') as f:
+        lines = f.readlines()
     
     # make fasta parser:
     record_list = []
     header = ''
-    sequence = ''
+    sequence = []
     for line in lines:
         line = line.strip()
         if line.startswith('>'):
             if header != "":
-                record_list.append([header, sequence])
-                sequence = ""
+                record_list.append([header.strip(), ''.join(sequence).strip()])
+                sequence = []
             header = line[1:]
         else:
-            sequence = sequence + line
-    record_list.append([header, sequence])
+            sequence.append(line)
+    record_list.append([header.strip(), ''.join(sequence).strip()])
 
     # clean output:
     for i in record_list:
--- src/get-subseqs.py
+++ src/get-subseqs.py
@@ -3,70 +3,31 @@ import numpy as np
 
 def get_subseqs():
 
-    try:
-        inFile_1 = sys.argv[1]
-        inFile_2 = sys.argv[2]
+    f1_lines = open(sys.argv[1]).readlines()
+    f2_lines = (sys.stdin if len(sys.argv) < 3 else open(sys.argv[2])).readlines()
 
-        with open(inFile_1,'r') as f1:
-            f1_lines = f1.readlines()
-        with open(inFile_2,'r') as f2:
-            f2_lines = f2.readlines()
+    record_list = []
+    header = ''
+    sequence = []
+    for line in f1_lines:
+        line = line.strip()
+        if line.startswith('>'):
+            if header != "":
+                record_list.append([header.strip(), ''.join(sequence).strip()])
+                sequence = []
+            header = line[1:]
+        else:
+            sequence.append(line)
+    record_list.append([header.strip(), ''.join(sequence).strip()])
+    record_list = np.array(record_list)
 
-        record_list = []
-        header = ''
-        sequence = ''
-        interval = []
-        for line in f1_lines:
-            line = line.strip()
-            if line.startswith('>'):
-                if header != "":
-                    record_list.append([header.strip(), sequence.strip()])
-                    sequence = ""
-                header = line[1:]
-            else:
-                sequence = sequence + line
-        record_list.append([header.strip(), sequence.strip()])
-        record_list = np.array(record_list)
-
-        for line in f2_lines:
-            chrom = line.split()[0]
-            start = int(line.split()[1])
-            end = int(line.split()[2])
-            find_seqs = np.where(np.array(record_list) == chrom)
-            seq = record_list[find_seqs[0]][0][1]
-            print(seq[start-1:end-1])
-            
-    except:
-        inFile_1 = sys.argv[1]
-        inFile_2 = sys.stdin
-
-        with open(inFile_1,'r') as f1:
-            f1_lines = f1.readlines()
-        f2_lines = inFile_2.readlines()
-
-        record_list = []
-        header = ''
-        sequence = ''
-        interval = []
-        for line in f1_lines:
-            line = line.strip()
-            if line.startswith('>'):
-                if header != "":
-                    record_list.append([header.strip(), sequence.strip()])
-                    sequence = ""
-                header = line[1:]
-            else:
-                sequence = sequence + line
-        record_list.append([header.strip(), sequence.strip()])
-        record_list = np.array(record_list)
-
-        for line in f2_lines:
-            chrom = line.split()[0]
-            start = int(line.split()[1])
-            end = int(line.split()[2])
-            find_seqs = np.where(np.array(record_list) == chrom)
-            seq = record_list[find_seqs[0]][0][1]
-            print(seq[start-1:end-1]) 
+    for line in f2_lines:
+        chrom = line.split()[0]
+        start = int(line.split()[1])
+        end = int(line.split()[2])
+        find_seqs = np.where(record_list == chrom)
+        seq = record_list[find_seqs[0]][0][1]
+        print(seq[start-1:end-1])
 
     return ''
 get_subseqs()
